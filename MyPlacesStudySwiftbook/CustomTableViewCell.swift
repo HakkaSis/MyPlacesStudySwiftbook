f065194c11d60118ab27bc446f35d19a82e01d31
Evgeny <skilioty@inbox.ru>
2020-05-18 12:39:12 +0300

    Custom TableViewCell

---
+++ MyPlacesStudySwiftbook/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlacesStudySwiftbook
+//
+//  Created by Евгений Скилиоти on 18.05.2020.
+//  Copyright © 2020 Евгений Скилиоти. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
