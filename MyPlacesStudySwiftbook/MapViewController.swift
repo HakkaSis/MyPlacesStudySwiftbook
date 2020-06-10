c33f06767b7e09065ba75026ad4dd3291d856af1
Evgeny <skilioty@inbox.ru>
2020-06-10 22:39:49 +0300

    Create Map view controller

---
+++ MyPlacesStudySwiftbook/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  MyPlacesStudySwiftbook
+//
+//  Created by Евгений Скилиоти on 10.06.2020.
+//  Copyright © 2020 Евгений Скилиоти. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func closeVC() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
